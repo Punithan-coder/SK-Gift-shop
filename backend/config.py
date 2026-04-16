5682bfbeed7b3c9a9d61ba162cf5c77072a4a38b
Punithan-coder <punithsekarpunithsekar@gmail.com>
2026-04-16 15:07:58 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -5,8 +5,4 @@ SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///skgiftsho
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 BCRYPT_LOG_ROUNDS = 12
 
-# Twilio settings for WhatsApp notifications
-TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
-TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
-TWILIO_WHATSAPP_NUMBER = os.environ.get('TWILIO_WHATSAPP_NUMBER')
-OWNER_WHATSAPP_NUMBER = '+918438529815'  # Your WhatsApp number
+
